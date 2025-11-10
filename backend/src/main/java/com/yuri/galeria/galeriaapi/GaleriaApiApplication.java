2e7eef17ff4e955923d3443ae9cab6a77452b329
yuri <ymendesqueiroz@gmail.com>
2025-11-10 16:52:57 -0300

    feat: configura projeto Spring Boot e conexão com PostgreSQL

---
+++ backend/src/main/java/com/yuri/galeria/galeriaapi/GaleriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.galeria.galeriaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaleriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaleriaApiApplication.class, args);
+	}
+
+}
