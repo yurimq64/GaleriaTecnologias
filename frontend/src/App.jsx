0b2d0b518b43412b28ed8ef8a9efad8eda219d79
yuri <ymendesqueiroz@gmail.com>
2025-11-11 15:32:40 -0300

    feat: conecta frontend à API e implementa o filtro dinâmico com useState e useEffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,40 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-    return (
+    const [tecnologias, setTecnologias] = useState([]);
+    const [termoBusca, setTermoBusca] = useState('');
 
+    const tecnologiasFiltradas = tecnologias.filter(tec =>
+        tec.nome.toLowerCase().includes(termoBusca.toLowerCase())
+    );
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/tecnologias')
+            .then(response => response.json())
+            .then(dados => setTecnologias(dados))
+            .catch(error => console.log('Erro ao buscar dados: ', error));
+    }, []);
+
+    return (
         <>
             <header>
                 <h1>Galeria de Tecnologias</h1>
-                <input type="text" id="campo-busca" placeholder="Filtrar por nome..."/>
+                <input
+                    type="text"
+                    id="campo-busca"
+                    placeholder="Filtrar por nome..."
+                    value={termoBusca}
+                    onChange={evento => setTermoBusca(evento.target.value)}
+                />
             </header>
 
             <main>
                 <div id="galeria-cards" className="galeria-container">
-                    {}
+                    {tecnologiasFiltradas.map(tec => (
+                        <div key={tec.id} className="card">
+                            <h2 className="card-nome">{tec.nome}</h2>
+                        </div>
+                    ))}
                 </div>
             </main>
         </>
