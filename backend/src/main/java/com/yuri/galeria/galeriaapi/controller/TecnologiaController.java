2c004803519039bf00a88facbd725b0833ce6643
yuri <ymendesqueiroz@gmail.com>
2025-11-11 12:49:08 -0300

    fix: habilita CORS no controller para acesso do frontend

--- backend/src/main/java/com/yuri/galeria/galeriaapi/controller/TecnologiaController.java
+++ backend/src/main/java/com/yuri/galeria/galeriaapi/controller/TecnologiaController.java
@@ -4,6 +4,7 @@ import com.yuri.galeria.galeriaapi.model.Tecnologia;
 import com.yuri.galeria.galeriaapi.repository.TecnologiaRepository;
 import com.yuri.galeria.galeriaapi.service.TecnologiaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tecnologias")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TecnologiaController {
 
     @Autowired
